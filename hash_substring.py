bcfc5fb2c17ba2c0810ca2817329cc505bfb38e4
vladislavantoniv <125074315+vladislavantoniv@users.noreply.github.com>
2023-04-16 18:27:04 +0000

    te

--- hash_substring.py
+++ hash_substring.py
@@ -5,6 +5,8 @@ def readd_input():
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
     choise=input();
     print(choise)
+    if choise.equals("I"):
+        print("Hello")
     if choise=='I':
         template=input();
         #ret_arr=template+" ";
