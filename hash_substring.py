acb7f93eabe69d5caab0328e9176ac2f4dd8de55
vladislavantoniv <125074315+vladislavantoniv@users.noreply.github.com>
2023-04-16 17:16:04 +0000

    first try

--- hash_substring.py
+++ hash_substring.py
@@ -1,19 +1,26 @@
 # python3
 
-def read_input():
+def readd_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
+    choise=input();
+    if choise=='I':
+        template=input();
+        string=input();
+    elif choise=='F':
+        f=open("tests/06","r");
+        template=f.readline();
+        string=f.readline();
+    ret_arr=template+" "+string;
+    return ret_arr
     # after input type choice
     # read two lines 
     # first line is pattern 
     # second line is text in which to look for pattern 
     
     # return both lines in one return
-    
     # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    #return (template.rstrip(), string.rstrip())
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
@@ -21,12 +28,47 @@ def print_occurrences(output):
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
-
+    pattern_hash=0;
+    text_hash=0;
+    h=1;
+    i=0;
+    j=0;
+    data=[];
+    for i in range(len(pattern)-1):
+        h=(h*256)%7
+    for i in range(len(pattern)):
+        pattern_hash=(pattern_hash*256+ord(pattern[i]))%7
+        text_hash=(text_hash*256+ord(text[i]))%7
+    for i in range(len(text)-len(pattern)+1):
+        if pattern_hash==text_hash:
+            for j in range(len(pattern)):
+                if text[i+j]==pattern[j]:
+                    j==j+1;
+                    print(pattern[j])
+                else:
+                    break;
+            if j==len(pattern):
+                data.append(i);
+        if i<(len(text)-len(pattern)):
+            text_hash=(256*(text_hash-ord(text[i])*h)+ord(text[i+len(pattern)]))%7;
+            if text_hash<0:
+                text_hash=text_hash+7;
+    k=0;
+    result_string="";
+    for k in enumerate (data):
+        result_string=result_string+data[k]+" "
+        print(result_string)
     # and return an iterable variable
-    return [0]
+    return data
 
 
 # this part launches the functions
 if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
+    arr=readd_input();
+    l=arr.split();
+    template=l[0];
+    string=l[1];
+    data=get_occurrences(template,string);
+
+    print_occurrences(data);
 
