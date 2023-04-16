61a36db6a26f80f28493d743ff0e96701e670046
vladislavantoniv <125074315+vladislavantoniv@users.noreply.github.com>
2023-04-16 18:00:21 +0000

    print_test_2

--- hash_substring.py
+++ hash_substring.py
@@ -4,8 +4,6 @@ def readd_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
     choise=input();
-    template="x";
-    string="h";
     if choise=='I':
         template=input();
         print(template)
